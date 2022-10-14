ad04a9155b53bcb6dde206c29520551ae1d36ca9
Rakshithac183 <103628971+Rakshithac183@users.noreply.github.com>
2022-10-14 12:47:38 +0530

    Add files via upload

---
+++ CloudArchitect/Course2/Project/psscript.ps1
@@ -0,0 +1,116 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\UdacityCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+
+#Function definitions
+
+Function Disable-InternetExplorerESC
+{
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force -ErrorAction SilentlyContinue -Verbose
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force -ErrorAction SilentlyContinue -Verbose
+    #Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green -Verbose
+}
+
+Function Enable-IEFileDownload
+{
+    $HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+    $HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+    Set-ItemProperty -Path $HKLM -Name "1803" -Value 0 -ErrorAction SilentlyContinue -Verbose
+    Set-ItemProperty -Path $HKCU -Name "1803" -Value 0 -ErrorAction SilentlyContinue -Verbose
+    Set-ItemProperty -Path $HKLM -Name "1604" -Value 0 -ErrorAction SilentlyContinue -Verbose
+    Set-ItemProperty -Path $HKCU -Name "1604" -Value 0 -ErrorAction SilentlyContinue -Verbose
+}
+
+Function Enable-CopyPageContent-In-InternetExplorer
+{
+    $HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+    $HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+    Set-ItemProperty -Path $HKLM -Name "1407" -Value 0 -ErrorAction SilentlyContinue -Verbose
+    Set-ItemProperty -Path $HKCU -Name "1407" -Value 0 -ErrorAction SilentlyContinue -Verbose
+}
+
+Function InstallChocolatey
+{   
+    #[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+    #[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+    $env:chocolateyUseWindowsCompression = 'true'
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -Verbose
+    choco feature enable -n allowGlobalConfirmation
+}
+
+Function DisableServerMgrNetworkPopup
+{
+    cd HKLM:\
+    New-Item -Path HKLM:\System\CurrentControlSet\Control\Network -Name NewNetworkWindowOff -Force 
+
+    Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+}
+
+Function CreateLabFilesDirectory
+{
+    New-Item -ItemType directory -Path C:\LabFiles -force
+}
+
+Function DisableWindowsFirewall
+{
+    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+}
+
+Function InstallEdgeChromium
+{
+    #Download and Install edge
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("http://go.microsoft.com/fwlink/?LinkID=2093437","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
+    sleep 5
+    
+    Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
+    sleep 5
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Azure Portal.lnk")
+    $Shortcut.TargetPath = """C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"""
+    $argA = """https://portal.azure.com"""
+    $Shortcut.Arguments = $argA 
+    $Shortcut.Save()
+
+    #Disable Welcome page of Microsoft Edge:
+
+    Set-Location hklm:
+    Test-Path .\Software\Policies\Microsoft
+    New-Item -Path .\Software\Policies\Microsoft -Name MicrosoftEdge
+    New-Item -Path .\Software\Policies\Microsoft\MicrosoftEdge -Name Main
+    New-ItemProperty -Path .\Software\Policies\Microsoft\MicrosoftEdge\Main -Name PreventFirstRunPage -Value "1" -Type DWORD -Force -ErrorAction SilentlyContinue | Out-Null
+
+    #Setting up the edge browser as default
+
+}
+
+Function InstallAzPowerShellModule
+{
+    <#Install-PackageProvider NuGet -Force
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    Install-Module Az -Repository PSGallery -Force -AllowClobber#>
+
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("https://github.com/Azure/azure-powershell/releases/download/v5.0.0-October2020/Az-Cmdlets-5.0.0.33612-x64.msi","C:\Packages\Az-Cmdlets-5.0.0.33612-x64.msi")
+    sleep 5
+    Start-Process msiexec.exe -Wait '/I C:\Packages\Az-Cmdlets-5.0.0.33612-x64.msi /qn' -Verbose 
+
+}
+
+#Function calls
+Disable-InternetExplorerESC
+Enable-IEFileDownload
+Enable-CopyPageContent-In-InternetExplorer
+InstallChocolatey
+DisableServerMgrNetworkPopup
+CreateLabFilesDirectory
+DisableWindowsFirewall
+InstallEdgeChromium
+InstallAzPowerShellModule
+
+
