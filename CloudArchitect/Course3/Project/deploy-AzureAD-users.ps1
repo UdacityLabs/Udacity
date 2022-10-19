d6a08f164b00f65adf3e9bb5a034edba0ca99c83
Rakshithac183 <103628971+Rakshithac183@users.noreply.github.com>
2022-10-19 18:41:36 +0530

    Add files via upload

---
+++ CloudArchitect/Course3/Project/deploy-AzureAD-users.ps1
@@ -0,0 +1,91 @@
+#Script to deploy Azure AD users
+Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
+Install-Module AzureAD -Force
+
+$AzureUserName = "<ENTER-YOUR-AZURE-USERNAME>"
+$password = "<ENTER-YOUR-AZURE-PASSWORD>"
+$AzureSubscriptionID = "<ENTER-YOUR-AZURE-SUBSCRIPTION-ID>"
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+
+Connect-AzureAD -Credential $cred | Out-Null
+
+
+Set-AzContext -SubscriptionId $AzureSubscriptionID
+
+#creating azure ad users
+
+$domainName = $AzureUserName.Split("@")[1]
+$tomusername = 'Tom@'+$domainName
+$andrew = 'Andrew@'+$domainName
+$srinadh = 'Srinadh@'+$domainName
+$neelima = 'Neelima@'+$domainName
+$Rajiv = 'Rajiv@'+$domainName
+$Debora = 'Debora@'+$domainName
+$Neha = 'Neha@'+$domainName
+$Sean = 'Sean@'+$domainName
+$Doug = 'Doug@'+$domainName
+$Igor = 'Igor@'+$domainName
+$Chris = 'Chris@'+$domainName
+$Karl = 'Karl@'+$domainName
+$Garrett = 'Garrett@'+$domainName
+$Sherrie = 'Sherrie@'+$domainName
+$Allison = 'Allison@'+$domainName
+$Kaitlyn = 'Kaitlyn@'+$domainName
+$Seth = 'Seth@'+$domainName
+$Lora = 'Lora@'+$domainName
+$Winifred = 'Winifred@'+$domainName
+$Michael = 'Michael@'+$domainName
+
+
+
+
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "Pass123!"
+$PasswordProfile.ForceChangePasswordNextLogin = $FALSE
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Tom" -PasswordProfile $PasswordProfile -MailNickName "tom-IT" -UserPrincipalName "$tomusername" -JobTitle "IT engineer" -Department "IT" -City "Detroit"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Andrew" -PasswordProfile $PasswordProfile -MailNickName "Andrew-IT" -UserPrincipalName "$andrew" -JobTitle "IT engineer" -Department "IT" -City "Charlotte"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Srinadh" -PasswordProfile $PasswordProfile -MailNickName "Srinadh-IT" -UserPrincipalName "$srinadh" -JobTitle "DBA" -Department "IT" -City "Detroit"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Neelima" -PasswordProfile $PasswordProfile -MailNickName "Neelima-IT" -UserPrincipalName "$neelima" -JobTitle "Dev Team Lead" -Department "IT" -City "Detroit"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Rajiv" -PasswordProfile $PasswordProfile -MailNickName "Rajiv-IT" -UserPrincipalName "$Rajiv" -JobTitle "Developer" -Department "IT" -City "Phoenix"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Debora" -PasswordProfile $PasswordProfile -MailNickName "Debora-IT" -UserPrincipalName "$Debora" -JobTitle "HR Operations Lead" -Department "IT" -City "Phoenix"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Neha" -PasswordProfile $PasswordProfile -MailNickName "Neha-IT" -UserPrincipalName "$Neha" -JobTitle "Dev DBA" -Department "IT" -City "Phoenix"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Sean" -PasswordProfile $PasswordProfile -MailNickName "Sean-IT" -UserPrincipalName "$Sean" -JobTitle "Web Designer" -Department "IT" -City "Detroit"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Doug" -PasswordProfile $PasswordProfile -MailNickName "Doug-IT" -UserPrincipalName "$Doug" -JobTitle "QA Tester" -Department "IT" -City "Remote"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Igor" -PasswordProfile $PasswordProfile -MailNickName "Igor" -UserPrincipalName "$Igor" -JobTitle "Financial Analyst" -Department "Finance" -City "Tampa"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Chris" -PasswordProfile $PasswordProfile -MailNickName "Chris" -UserPrincipalName "$Chris" -JobTitle "CIO" -Department "Executive Staff" -City "Charlotte"
+
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Karl" -PasswordProfile $PasswordProfile -MailNickName "Karl" -UserPrincipalName "$Karl" -JobTitle "COO" -Department "Executive Staff" -City "Charlotte"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Garrett" -PasswordProfile $PasswordProfile -MailNickName "Garrett" -UserPrincipalName "$Garrett" -JobTitle "Driver" -Department "Service Management" -City "Remote"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Sherrie" -PasswordProfile $PasswordProfile -MailNickName "Sherrie" -UserPrincipalName "$Sherrie" -JobTitle "Salesperson" -Department "Sales" -City "Remote"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Allison" -PasswordProfile $PasswordProfile -MailNickName "Allison" -UserPrincipalName "$Allison" -JobTitle "Legal Researcher" -Department "Legal" -City "Phoenix"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Kaitlyn" -PasswordProfile $PasswordProfile -MailNickName "Kaitlyn" -UserPrincipalName "$Kaitlyn" -JobTitle "HR Admin" -Department "Human Resources" -City "Tampa"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Lora" -PasswordProfile $PasswordProfile -MailNickName "Lora" -UserPrincipalName "$Lora" -JobTitle "HR Manager" -Department "Human Resources" -City "Charlotte"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Seth" -PasswordProfile $PasswordProfile -MailNickName "Seth" -UserPrincipalName "$Seth" -JobTitle "Azure Architect" -Department "IT" -City "Charlotte"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Winifred" -PasswordProfile $PasswordProfile -MailNickName "Winifred" -UserPrincipalName "$Winifred" -JobTitle "Support Desk Manager" -Department "IT" -City "Charlotte"
+
+New-AzureADUser -AccountEnabled $True -DisplayName "Michael" -PasswordProfile $PasswordProfile -MailNickName "Michael" -UserPrincipalName "$Michael" -JobTitle "Support Desk Admin" -Department "IT" -City "Detroit"
+
+
+
+
